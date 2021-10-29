b0241bfc58c560f69d519ea1f5c39fa4328fac13
Farshid-997 <arnob.farshid@gmail.com>
2021-10-30 01:06:03 +0600

    commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Components/Header/Header';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Location from './Components/Location/Location';
+import Packages from './Components/Packages/Packages';
+import Blog from './Components/Blog/Blog';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
+import Contact from './Components/Contact/Contact'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+     <BrowserRouter>
+     <Header></Header>
+     <Switch>
+       <Route path='/'>
+<Home></Home>
+       </Route>
+
+       <Route path="/home">
+<Home></Home>
+       </Route>
+       <Route path="/location">
+<Location></Location>
+       </Route>
+
+       <Route path="/packages">
+<Packages></Packages>
+       </Route>
+
+
+       <Route path="/blog">
+<Blog></Blog>
+       </Route>
+
+       <Route path="/contact">
+<Contact></Contact>
+       </Route>
+
+       <Route path="/login">
+            <Login></Login>
+          </Route>
+
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+
+     </Switch>
+     
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="header">
+            <nav>
+            <NavLink to="/home">Home</NavLink>
+                <NavLink to="/location">Location</NavLink>
+                <NavLink to="/packages">Packages</NavLink>
+                
+                <NavLink to="/blog">Blog</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+                <NavLink to="/register">Register</NavLink>
+                <NavLink to="/login">Login</NavLink>
+
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
